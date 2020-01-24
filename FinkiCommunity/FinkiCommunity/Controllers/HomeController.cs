deb9b4d1b5390e28b35d65ce44e41c1cc1f85940
FisnikL <fisnik.limani1@gmail.com>
2020-01-24 23:37:27 +0100

    add search inputs in views

--- FinkiCommunity/FinkiCommunity/Controllers/HomeController.cs
+++ FinkiCommunity/FinkiCommunity/Controllers/HomeController.cs
@@ -9,34 +9,39 @@ namespace FinkiCommunity.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext db = new ApplicationDbContext();
-        public ActionResult Index()
+        public ActionResult Index(string term)
         {
+            List<HomeGroupModel> homePageGroups = new List<HomeGroupModel>();
+            List<HomePostModel> homePagePosts = new List<HomePostModel>();
 
-            List<HomeGroupModel> homePageGroups = db.Groups.Select(group => new HomeGroupModel
+            if (term == null)
             {
-                CourseCode = group.CourseCode,
-                CourseName = group.CourseName,
-                CourseDescription = group.CourseDescription,
-                CoursePictureUrl = group.CoursePictureUrl
-            }).ToList();
-
-
-            List<HomePostModel> homePagePosts = db.Posts
-                .Include(p => p.UserOwner)
-                .Include(p => p.Group)
-                .OrderByDescending(p => p.Created)
-                .Select(post => new HomePostModel()
+                homePageGroups = db.Groups.Select(group => new HomeGroupModel
                 {
-                    Id = post.Id,
-                    Title = post.Title,
-                    Content = post.Content,
-                    Created = post.Created,
-                    NumberOfLikes = post.NumberOfLikes,
-                    NumberOfReplies = post.NumberOfReplies,
-                    UserOwner = post.UserOwner,
-                    Group = post.Group
+                    CourseCode = group.CourseCode,
+                    CourseName = group.CourseName,
+                    CourseDescription = group.CourseDescription,
+                    CoursePictureUrl = group.CoursePictureUrl
                 }).ToList();
 
+
+                homePagePosts = db.Posts
+                    .Include(p => p.UserOwner)
+                    .Include(p => p.Group)
+                    .OrderByDescending(p => p.Created)
+                    .Select(post => new HomePostModel()
+                    {
+                        Id = post.Id,
+                        Title = post.Title,
+                        Content = post.Content,
+                        Created = post.Created,
+                        NumberOfLikes = post.NumberOfLikes,
+                        NumberOfReplies = post.NumberOfReplies,
+                        UserOwner = post.UserOwner,
+                        Group = post.Group
+                    }).ToList();
+            }
+            
 
             HomePageModel model = new HomePageModel()
             {
@@ -47,11 +52,15 @@ namespace FinkiCommunity.Controllers
             return View(model);
         }
 
-        [HttpPost]
         public ActionResult SearchGroups(string term)
         {
             //return View();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult SearchPosts(string term)
+        {
+            return View();
         }
     }
 }
