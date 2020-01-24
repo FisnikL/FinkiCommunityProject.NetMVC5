719f9133b2d5dda888a5df9e04535d9a0cd83c0e
FisnikL <fisnik.limani1@gmail.com>
2020-01-24 23:47:18 +0100

    add search groups & posts options in home page

--- FinkiCommunity/FinkiCommunity/Controllers/HomeController.cs
+++ FinkiCommunity/FinkiCommunity/Controllers/HomeController.cs
@@ -9,38 +9,33 @@ namespace FinkiCommunity.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext db = new ApplicationDbContext();
-        public ActionResult Index(string term)
+        public ActionResult Index()
         {
-            List<HomeGroupModel> homePageGroups = new List<HomeGroupModel>();
-            List<HomePostModel> homePagePosts = new List<HomePostModel>();
 
-            if (term == null)
+            List<HomeGroupModel>homePageGroups = db.Groups.Select(group => new HomeGroupModel
             {
-                homePageGroups = db.Groups.Select(group => new HomeGroupModel
+                CourseCode = group.CourseCode,
+                CourseName = group.CourseName,
+                CourseDescription = group.CourseDescription,
+                CoursePictureUrl = group.CoursePictureUrl
+            }).ToList();
+
+
+            List<HomePostModel> homePagePosts = db.Posts
+                .Include(p => p.UserOwner)
+                .Include(p => p.Group)
+                .OrderByDescending(p => p.Created)
+                .Select(post => new HomePostModel()
                 {
-                    CourseCode = group.CourseCode,
-                    CourseName = group.CourseName,
-                    CourseDescription = group.CourseDescription,
-                    CoursePictureUrl = group.CoursePictureUrl
+                    Id = post.Id,
+                    Title = post.Title,
+                    Content = post.Content,
+                    Created = post.Created,
+                    NumberOfLikes = post.NumberOfLikes,
+                    NumberOfReplies = post.NumberOfReplies,
+                    UserOwner = post.UserOwner,
+                    Group = post.Group
                 }).ToList();
-
-
-                homePagePosts = db.Posts
-                    .Include(p => p.UserOwner)
-                    .Include(p => p.Group)
-                    .OrderByDescending(p => p.Created)
-                    .Select(post => new HomePostModel()
-                    {
-                        Id = post.Id,
-                        Title = post.Title,
-                        Content = post.Content,
-                        Created = post.Created,
-                        NumberOfLikes = post.NumberOfLikes,
-                        NumberOfReplies = post.NumberOfReplies,
-                        UserOwner = post.UserOwner,
-                        Group = post.Group
-                    }).ToList();
-            }
             
 
             HomePageModel model = new HomePageModel()
@@ -52,15 +47,80 @@ namespace FinkiCommunity.Controllers
             return View(model);
         }
 
-        public ActionResult SearchGroups(string term)
+        public ActionResult SearchGroups(string groupTerm)
         {
-            //return View();
-            return RedirectToAction("Index");
+            List<HomeGroupModel> homePageGroups = db.Groups
+                 .Where(g => g.CourseName.Contains(groupTerm) || g.CourseDescription.Contains(groupTerm) || g.CourseDescription.Contains(groupTerm))
+                .Select(group => new HomeGroupModel
+                {
+                    CourseCode = group.CourseCode,
+                    CourseName = group.CourseName,
+                    CourseDescription = group.CourseDescription,
+                    CoursePictureUrl = group.CoursePictureUrl
+                }).ToList();
+
+
+            List<HomePostModel> homePagePosts = db.Posts
+                .Include(p => p.UserOwner)
+                .Include(p => p.Group)
+                .OrderByDescending(p => p.Created)
+                .Select(post => new HomePostModel()
+                {
+                    Id = post.Id,
+                    Title = post.Title,
+                    Content = post.Content,
+                    Created = post.Created,
+                    NumberOfLikes = post.NumberOfLikes,
+                    NumberOfReplies = post.NumberOfReplies,
+                    UserOwner = post.UserOwner,
+                    Group = post.Group
+                }).ToList();
+
+            HomePageModel model = new HomePageModel()
+            {
+                HomeGroups = homePageGroups,
+                HomePosts = homePagePosts
+            };
+            
+            return View("Index", model);
         }
 
-        public ActionResult SearchPosts(string term)
+        public ActionResult SearchPosts(string postTerm)
         {
-            return View();
+            List<HomeGroupModel> homePageGroups = db.Groups
+                .Select(group => new HomeGroupModel
+                {
+                    CourseCode = group.CourseCode,
+                    CourseName = group.CourseName,
+                    CourseDescription = group.CourseDescription,
+                    CoursePictureUrl = group.CoursePictureUrl
+                }).ToList();
+
+
+            List<HomePostModel> homePagePosts = db.Posts
+                .Where(p => p.Title.Contains(postTerm) || p.Content.Contains(postTerm))
+                .Include(p => p.UserOwner)
+                .Include(p => p.Group)
+                .OrderByDescending(p => p.Created)
+                .Select(post => new HomePostModel()
+                {
+                    Id = post.Id,
+                    Title = post.Title,
+                    Content = post.Content,
+                    Created = post.Created,
+                    NumberOfLikes = post.NumberOfLikes,
+                    NumberOfReplies = post.NumberOfReplies,
+                    UserOwner = post.UserOwner,
+                    Group = post.Group
+                }).ToList();
+
+            HomePageModel model = new HomePageModel()
+            {
+                HomeGroups = homePageGroups,
+                HomePosts = homePagePosts
+            };
+
+            return View("Index", model);
         }
     }
 }
